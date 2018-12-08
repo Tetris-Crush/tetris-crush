b18a8948fa4d0f2921b2eebedcb09c02f50c62e1
l40011-uevora <43813431+l40011-uevora@users.noreply.github.com>
2018-12-08 21:22:19 +0000

    Version 3.0 (Beta)
    Implemented unit testing using GUT (Godot Unit Testing).

--- board.gd
+++ board.gd
@@ -5,17 +5,52 @@ var boardState = []
 func _ready():
     pass
 
-func add_boardState(arr):
-    #adiciona a posição final de uma peça à lista boardState
-    pass
+func add_board_state(arr):
+    boardState.append(arr)
 
-func boardLength():
-    var length = boardState.size()
-    return length
+func destroyer(arr): #arr é a blockInfo [posição x, posição y, cor]
+    var originalX = arr[0]
+    var originalY = arr[1]
+    var to_be_destroyed = [arr]
+    
+    while arr[0] < 368: #ir para a direita procurar peças iguais 
+        var trigger1 = 0  #funciona como sinal para garantir que só destroi se forem seguidas
+        arr[0]+=67
+        if arr in boardState:
+            to_be_destroyed.append(arr)
+            trigger1 = 1
+        if trigger1 == 0:
+            break
+    
+    arr[0]=originalX
+    
+    while arr[0] > 33: #ir para a esquerda
+        var trigger2 = 0
+        arr[0]-=67
+        if arr in boardState:
+            to_be_destroyed.append(arr)
+            trigger2 = 1
+        if trigger2 == 0:
+            break
 
+    arr[0] = originalX
 
-func destroyer():
-    #destroi as peças se tiver 3 iguais ligadas
-    pass
+    while arr[1] < 566: #ir para baixo
+        var trigger3 = 0
+        arr[1]+=66
+        if arr in boardState:
+            to_be_destroyed.append(arr)
+            trigger3 = 1
+        if trigger3 == 0:
+            break
+    
+    arr[1] = originalY
+    return to_be_destroyed
 
-
+func blocks_to_release(arr): #arr é a func destroyer(arr)
+    var releaseBlocks = []
+    for i in range(boardState.size()):
+        for j in range(arr.size()):
+            if arr[j]==boardState[i]:
+                releaseBlocks.append[i]
+    return releaseBlocks
--- player.gd
+++ player.gd
@@ -1,18 +1,34 @@
 extends Node
 
 var board = load("res://board.tscn").instance()
-var boardScript = load("res://board.gd").new()
-var blockScript = load("res://block.gd").new()
 
-func _ready():
-    var block = load("res://block.tscn").instance()  
-    add_child(block)
+func _ready():  
     add_child(board)
+    add_block()
 
 func _process(delta):
-    
-    if Input.is_action_just_released("ui_up"):
-        boardScript.add_boardState(blockScript.get_block_info())
-        var block = load("res://block.tscn").instance()  
-        add_child(block)
+    if game_running():
+        board.add_board_state(active_block().blockInfo)
+        board.same_color_pieces(active_block().blockInfo)
+        end_game(active_block().blockInfo[1])
+        add_block()
+    if Input.is_action_just_pressed("ui_cancel"):
+        exit_game()
+
+func add_block():
+    var block = load("res://block.tscn").instance()
+    add_child(block)
+
+func game_running():
+    return !(active_block().is_physics_processing())
+
+func end_game(posY):
+    if posY == 104:
+        exit_game()
+
+func active_block():
+    return get_child(get_child_count()-1)
+
+func exit_game():
+    get_tree().quit()
 
