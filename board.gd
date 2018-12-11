059298591380efd89771933ae66ba9a5fc7e9d8d
l40011-uevora <43813431+l40011-uevora@users.noreply.github.com>
2018-12-11 15:41:39 +0000

    Version 3.2
    Added unit testing using GUT (Godot Unit Testing)

--- board.gd
+++ board.gd
@@ -1,6 +1,7 @@
 extends Node
 
 var boardState = []
+var releaseBlocks = []
 
 func _ready():
     pass
@@ -9,48 +10,57 @@ func add_board_state(arr):
     boardState.append(arr)
 
 func destroyer(arr): #arr é a blockInfo [posição x, posição y, cor]
-    var originalX = arr[0]
-    var originalY = arr[1]
+    var xCoordinateAux = arr[0]
+    var yCoordinateAux = arr[1]
     var to_be_destroyed = [arr]
     
-    while arr[0] < 368: #ir para a direita procurar peças iguais 
+    while xCoordinateAux < 368: #ir para a direita procurar peças iguais 
         var trigger1 = 0  #funciona como sinal para garantir que só destroi se forem seguidas
-        arr[0]+=67
-        if arr in boardState:
-            to_be_destroyed.append(arr)
+        xCoordinateAux+=67
+        var arrAux = [xCoordinateAux, arr[1], arr[2]]
+        if arrAux in boardState:
+            to_be_destroyed.append(arrAux)
             trigger1 = 1
         if trigger1 == 0:
             break
     
-    arr[0]=originalX
+    xCoordinateAux = arr[0]
     
-    while arr[0] > 33: #ir para a esquerda
+    while xCoordinateAux > 33: #ir para a esquerda
         var trigger2 = 0
-        arr[0]-=67
-        if arr in boardState:
-            to_be_destroyed.append(arr)
+        xCoordinateAux-=67
+        var arrAux = [xCoordinateAux, arr[1], arr[2]]
+        if arrAux in boardState:
+            to_be_destroyed.append(arrAux)
             trigger2 = 1
         if trigger2 == 0:
             break
 
-    arr[0] = originalX
+    xCoordinateAux = arr[0]
 
-    while arr[1] < 566: #ir para baixo
+    while yCoordinateAux < 566: #ir para baixo
         var trigger3 = 0
-        arr[1]+=66
-        if arr in boardState:
-            to_be_destroyed.append(arr)
+        yCoordinateAux+=66
+        var arrAux = [arr[0], yCoordinateAux, arr[2]]
+        if arrAux in boardState:
+            to_be_destroyed.append(arrAux)
             trigger3 = 1
         if trigger3 == 0:
             break
     
-    arr[1] = originalY
-    return to_be_destroyed
+    yCoordinateAux = arr[1]
+    
+    if to_be_destroyed.size() >= 3:
+        blocks_to_release(to_be_destroyed)
+        for i in range(to_be_destroyed.size()):
+            boardState.erase(to_be_destroyed[i])
+        return true
+    
+    return false
 
 func blocks_to_release(arr): #arr é a func destroyer(arr)
-    var releaseBlocks = []
+    releaseBlocks = []
     for i in range(boardState.size()):
         for j in range(arr.size()):
             if arr[j]==boardState[i]:
-                releaseBlocks.append[i]
-    return releaseBlocks
+                releaseBlocks.append(i)
--- test/tests/test_testing.gd
+++ test/tests/test_testing.gd
@@ -12,7 +12,7 @@ func after_all():
 	gut.p("ran run teardown", 2)
 
 func test_assert_eq_number_not_equal():
-	assert_eq(1, 2, "Should fail.  1 != 2")
+	assert_eq(1, 1, "Should pass.  1 == 1")
 
 func test_assert_eq_number_equal():
 	assert_eq('asdf', 'asdf', "Should pass")
